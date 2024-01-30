f8242e51df5fbdbda2ba6ff4fc6b3a630441818c
Alexandra <alexandramoise952@gmail.com>
2024-01-30 14:59:05 +0200

    started linking to backend (to delete irrelevant components and stuff)

---
+++ src/services/doctor_service.js
@@ -0,0 +1,34 @@
+const API_URL = "http://localhost:8080/doctors";
+
+async function createDoctorAccount(email) {
+    let connectionError = false;
+    let response;
+
+    console.log("Incepe crearea: ", email);
+    try {
+
+        response = await fetch(`${API_URL}/new-doctor-account?email=${email}`, {
+            method: "POST",
+        });
+    } catch (error) {
+        connectionError = true;
+    }
+
+    if (connectionError) {
+        throw new Error("Server connection error");
+    }
+
+    const result = await response.json();
+    if (!response.ok) {
+        throw new Error(result.message);
+    } else {
+        console.log("In service s-a primit: ", result.email);
+        localStorage.setItem("email", result.email);
+    }
+
+    return result;
+}
+
+export {
+    createDoctorAccount
+};
---
+++ src/views/InitiateAccountView.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { createDoctorAccount } from '../services/doctor_service.js'
+import { ref } from 'vue';
+
+const emailText = ref('');
+
+async function ceva() {
+    if (emailText.value === '' || emailText.value === null) {
+        alert("Input can't be empty");
+    } else {
+        try {
+            const result = await createDoctorAccount(emailText.value);
+        } catch (error) {
+            alert(error.message);
+            console.error(error);
+        }
+    }
+}
+</script>
+
+<template>
+    <h1>Creare cont</h1>
+    <input v-model="emailText">
+    <button @click="ceva">Creare</button>
+</template>
+
+
+<style scoped>
+
+</style>
